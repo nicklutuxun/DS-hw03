4e6baaee651d81167916e5cd0a8efdb5f4eda796
Nick Lu <tlu32@jhu.edu>
2022-02-22 15:30:46 -0500

    Initial commit

---
+++ src/main/java/hw3/sort/InsertionSort.java
@@ -0,0 +1,23 @@
+package hw3.sort;
+
+
+import hw3.list.IndexedList;
+
+/**
+ * The Insertion Sort algorithm, with minimizing swaps optimization.
+ *
+ * @param <T> Element type.
+ */
+public final class InsertionSort<T extends Comparable<T>>
+    implements SortingAlgorithm<T> {
+
+  @Override
+  public void sort(IndexedList<T> indexedList) {
+    // TODO: Implement me
+  }
+
+  @Override
+  public String name() {
+    return "Insertion Sort";
+  }
+}
